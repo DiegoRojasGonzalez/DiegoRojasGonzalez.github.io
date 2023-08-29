7f9b110d5e31fdd682679237ec0c28f04694bb08
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2023-08-29 18:38:17 -0400

    fix : change projects component elements[gif]

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -21,7 +21,7 @@ export class ProjectsComponent implements OnInit {
   elements = [
     {
       image: 'https://raw.githubusercontent.com/DiegoRojasGonzalez/Resources/main/ss-task-wise-light.png',
-      gif: '../assets/glass.gif',
+      gif: 'https://raw.githubusercontent.com/DiegoRojasGonzalez/Resources/main/ss-task-wise-light.png',
       title: 'Task wise',
       description: 'Task-Wise application, designed to simplify your task management process. With this user-friendly app, you can easily create, organize, and prioritize tasks that need your attention. Whether its for personal use, work-related projects, or team collaboration, our app offers a straightforward and intuitive platform for jotting down your tasks.',
       technologies: ['https://img.icons8.com/?size=512&id=uJM6fQYqDaZK&format=png', 'https://img.icons8.com/?size=512&id=71257&format=png'],
@@ -30,7 +30,7 @@ export class ProjectsComponent implements OnInit {
     },
     {
       image: 'https://raw.githubusercontent.com/DiegoRojasGonzalez/Resources/main/ss-focus-nice.png',
-      gif: '../assets/glass1.gif',
+      gif: 'https://raw.githubusercontent.com/DiegoRojasGonzalez/Resources/main/ss-focus-nice.png',
       title: 'Focus Nice',
       description: 'Focus Nice is a Pomodoro app developed in Python using Kvlang (Kivy/KivyMD) framework. The app is designed to enhance productivity and maintain a focused approach to tasks by implementing the Pomodoro technique. It allows users to divide their time into intervals of work and rest, helping them maintain a proper balance and avoid mental fatigue.',
       technologies: ['https://img.icons8.com/?size=512&id=13441&format=png','https://camo.githubusercontent.com/e08cad103f2f532b4d9aeee9fba90588afc9e8c5ef3b0f6524fce2e0be540c6a/68747470733a2f2f7175696e746167726f75702e636f6d2f636d732f707974686f6e2f696d616765732f6b6976792d6c6f676f2e706e672f4040696d616765732f35323035366238372d353837322d346532342d383438332d3263323065363266613739642e706e67'],
