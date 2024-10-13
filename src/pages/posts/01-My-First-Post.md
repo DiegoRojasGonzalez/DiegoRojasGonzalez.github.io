1c0fde6f1c03f86ebb0950eaffcc29f14117426e
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-10-13 17:18:29 -0300

    feat: :sparkles: improve md files creation and indexings articles section

---
+++ src/pages/posts/01-My-First-Post.md
@@ -0,0 +1,34 @@
+---
+title: "The greatest post of all time"
+date:
+  day: 13
+  month: 10
+  year: 2024
+description: "This post if created on 10/13/2024 when i implement this feature on portfolio "
+readingTime: 2
+tags: ["first", "tag2"]
+
+layout: ../../layouts/BaseLayout.astro
+---
+
+# This is my first post
+
+Im testing implementation of a blog postings
+
+- **bold** and _italics._
+- lists
+- [links](https://astro.build)
+- **inline code**
+- and more!
+
+```kt
+//code example
+val foo = "bar"
+
+```
+
+| Table Example                                |                                              |
+| -------------------------------------------- | -------------------------------------------- |
+| ![Pet](https://picsum.photos/id/238/200/300) | ![Pet](https://picsum.photos/id/238/200/300) |
+
+`other code example`
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              semibold: 600,
+              fontSize: "1.875rem",
+              lineHeight: "2.25rem",
+            },
+            h2: {
+              semibold: 600,
+              fontSize: "1.875rem",
+              lineHeight: "2.25rem",
+            },
+            p: {
+              fontSize: "1.125rem",
+              lineHeight: "1.75rem",
+            },
+            li: {
+              fontSize: "1.125rem",
+              lineHeight: "1.75rem",
+            },
+          },
+        },
+      },
+    },
+  },
+
+  plugins: [require("@tailwindcss/typography")],
+};
