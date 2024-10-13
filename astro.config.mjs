b9fb3f3a326bb6a991119a99d8d9cf3035f91832
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-10-13 18:02:48 -0300

    ci: :green_heart: update ci for gh

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 import robotsTxt from "astro-robots-txt";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: "https://diegorojasgonzalez.github.io/",
+  base: "DiegoRojasGonzalez.github.io",
 });
